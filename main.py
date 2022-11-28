f1001b763af7594a101e37da33a07402fde3119e
kseansi <ksansi@yandex.ru>
2022-11-28 13:46:55 +0500

    1

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 async def on_startup(dp):
 
-    # from utils.notify_admins import on_startup_notify
-    # await on_startup_notify(dp)
-
-
-    # from utils.set_bot_commands import set_default_commands
-    # await set_default_commands(dp)
-
 
     print("Бот запущен")
 
